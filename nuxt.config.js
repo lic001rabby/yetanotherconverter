7920954f64b57c57cf818b821ce245ddc217a506
Fazly Rabby <kmfrabby@gmail.com>
2021-11-02 17:44:06 +0600

    fix: server vars

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,11 @@ export default {
       return routes
     },
   }, */
+  dev: process.env.NODE_ENV !== 'production',
+  server: {
+    port: process.env.PORT || 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
   // Target: https://go.nuxtjs.dev/config-target
   target: 'server',
 
